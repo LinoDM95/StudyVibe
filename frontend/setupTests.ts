13ea02e87f2cfe6802aa147a6336f5fef3aeb89d
LDM158 <linodemarco95@gmail.com>
2025-02-14 15:11:19 +0100

    Aktualisiere CI-Workflow

--- frontend/setupTests.ts
+++ frontend/setupTests.ts
@@ -1,5 +1,6 @@
 //setupTest.ts
 
+import '@testing-library/jest-dom';
 import { afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
