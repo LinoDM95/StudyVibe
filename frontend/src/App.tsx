cb6f1b774e57687b8533bd6c146986fbf1ec3ec6
LDM158 <linodemarco95@gmail.com>
2025-02-18 12:57:23 +0100

    Added fonts and different sizes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,19 +3,12 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="bg-studyvibe-red text-studyvibe-white p-4 rounded-md">
-        wefwefewfwf
-      </div>
-      <div className="bg-studyvibe-yellow2 text-studyvibe-white p-4 rounded-md">
-        Test-Text mit StudyVibe Farben
-      </div>
-
-      <button className="bg-studyvibe-red text-white px-4 py-2 rounded">
-        Warn-Button
-      </button>
-      <div className="bg-studyvibe-teal2">
-        fwfwefwe
-      </div>
+      <p className="font-inter text-h1">fwefwefwefw</p>
+      <p className="font-intertight text-h1">fwefwefwefw</p>
+      <p className="font-manrope text-h1">fwefwefwefw</p>
+      <button className="font-intertight text-h1">fwefwefwefw</button>
+      <button className="font-inter text-h1">fwefwefwefw</button>
+      <button className="font-manrobuttone text-h1">fwefwefwefw</button>
     </>
   );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-
+      fontFamily: {
+        "inter-tight": ['"Inter Tight"', "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+        manrope: ["Manrope", "sans-serif"],
+      },
+      fontSize: {
+        h1: "44px",
+        h2: "35px",
+        h3: "30px",
+        h4: "30px",
+        subtitle: "20px",
+        menu: "18px",
+        button: "16px",
+        buttonSmall: "14px",
+        body: "16px",
+        body2: "14px",
+      },
     },
   },
-  plugins: [],
 };
