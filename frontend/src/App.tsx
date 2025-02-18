9cc163f50a77e04e32303fa6136fdbb4a5463e2b
LDM158 <linodemarco95@gmail.com>
2025-02-18 13:42:20 +0100

    Minor adjustments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <p className="font-inter text-h1">fwefwefwefw</p>
-      <p className="font-intertight text-h1">fwefwefwefw</p>
-      <p className="font-manrope text-h1">fwefwefwefw</p>
-      <button className="font-intertight text-h1">fwefwefwefw</button>
-      <button className="font-inter text-h1">fwefwefwefw</button>
-      <button className="font-manrobuttone text-h1">fwefwefwefw</button>
+
     </>
   );
 }
