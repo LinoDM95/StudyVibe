d2b0425b944a2a07af4c855405eb3f8118c8a9ca
LDM158 <linodemarco95@gmail.com>
2025-02-21 19:04:21 +0100

    Sync

---
+++ frontend/src/components/layouts/homepage_layouts/learning_path_section.tsx
@@ -0,0 +1,15 @@
+import Timeline from "../../ui/timeline";
+
+interface LearningPathSectionProps {}
+
+const LearningPathSection: React.FC<LearningPathSectionProps> = ({}) => {
+
+
+  return (
+<div>
+  <Timeline />
+</div>
+  );
+};
+
+export default LearningPathSection;
--- frontend/src/pages/homepage.tsx
+++ frontend/src/pages/homepage.tsx
@@ -2,6 +2,7 @@
 import HeroSection from "../components/layouts/homepage_layouts/hero_section";
 import CallToAction from "../components/layouts/homepage_layouts/call_to_action_section";
 import LPRecommendations from "../components/layouts/homepage_layouts/learning_path_recommendations";
+import LearningPathSection from "../components/layouts/homepage_layouts/learning_path_section";
 
 const Homepage = () => {
   return (
@@ -9,6 +10,7 @@ const Homepage = () => {
       <HeroSection />
       <CallToAction />
       <LPRecommendations />
+      <LearningPathSection />
     </div>
   );
 };
