d210e9181c8d7a66e9dc5c097e893b5f6cbff817
LDM158 <linodemarco95@gmail.com>
2025-02-24 12:23:46 +0100

    Designed header dropdown section

--- frontend/src/components/pop_ups/drop_down_coursepaths.tsx
+++ frontend/src/components/pop_ups/drop_down_coursepaths.tsx
@@ -1,5 +1,10 @@
 import DropDownBase from "./drop_down_base";
 
+import Math from "../../assets/icons/Math.svg?react";
+import Physics from "../../assets/icons/Physics.svg?react";
+import ComputerScience from "../../assets/icons/ComputerScience.svg?react";
+import GraduateHat from "../../assets/icons/GraduateHat.svg?react";
+
 interface DropDownCoursePathsProps {
   openStatus: boolean;
   onChange?: (isActive: boolean) => void;
@@ -12,47 +17,59 @@ const DropDownCoursePaths: React.FC<DropDownCoursePathsProps> = ({
   return (
     <div className="flex w-auto">
       <DropDownBase openStatus={openStatus} onChange={onChange}>
-        <div className="flex">
-          <div>
-            <div>
-              <h1 className="font-intertight">Mathe</h1>
+        <div className="flex flex-col gap-5">
+          <div className="flex">
+            <div className="flex gap-3">
+              <Math />
+              <div className="flex flex-col gap-3">
+                <h1 className="font-intertight">Mathe</h1>
+                <nav>
+                  <ul className="flex flex-col gap-1 font-manrope">
+                    <li>Vektoren</li>
+                    <li>Integralrechnung</li>
+                    <li>Funktionen</li>
+                  </ul>
+                </nav>
+              </div>
             </div>
-            <nav>
-              <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-              </ul>
-            </nav>
-          </div>
 
-          <div>
-            <div>
-              <h1 className="font-intertight">Physik</h1>
+            <div className="flex gap-3">
+              <Physics />
+              <div className="flex flex-col gap-3">
+                <h1 className="font-intertight">Physik</h1>
+                <nav>
+                  <ul className="flex flex-col gap-1 font-manrope">
+                    <li>Vektoren</li>
+                    <li>Integralrechnung</li>
+                    <li>Funktionen</li>
+                  </ul>
+                </nav>
+              </div>
             </div>
-            <nav>
-              <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-              </ul>
-            </nav>
-          </div>
 
-          <div>
-            <div>
-              <h1 className="font-intertight">Informatik</h1>
+            <div className="flex gap-3">
+              <ComputerScience />
+              <div className="flex flex-col gap-3">
+                <h1 className="font-intertight">Informatik</h1>
+                <nav>
+                  <ul className="flex flex-col gap-1 font-manrope">
+                    <li>Vektoren</li>
+                    <li>Integralrechnung</li>
+                    <li>Funktionen</li>
+                  </ul>
+                </nav>
+              </div>
             </div>
+          </div>
+          <div className="flex items-center gap-3">
+            <GraduateHat />
             <nav>
               <ul>
-                <li></li>
-                <li></li>
-                <li></li>
+                <li className="font-intertight">Alle Lernpfade</li>
               </ul>
             </nav>
           </div>
         </div>
-        <div>Alle Lernpfade</div>
       </DropDownBase>
     </div>
   );
